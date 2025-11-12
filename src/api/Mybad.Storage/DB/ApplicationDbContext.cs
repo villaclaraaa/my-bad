577cf8edead165d53fba22922c9ad0c6ea4fcf68
villaclara <villaclara1231@gmail.com>
2025-11-12 13:45:16 +0200

    s

---
+++ src/api/Mybad.Storage/DB/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mybad.Storage.DB.Entities;
+
+namespace Mybad.Storage.DB;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Ward> Wards { get; set; } = default!;
+}
