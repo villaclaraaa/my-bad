f8744dbc456436f08d0afa2ae5ffe32ea2d1a3e4
villaclara <villaclara1231@gmail.com>
2025-11-28 02:10:41 +0200

    landing page setup

--- src/ui/front/Mybad.AngularFront/src/app/app-routing.module.ts
+++ src/ui/front/Mybad.AngularFront/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { IndexComponent } from './index/index.component';
 
 // Here we should probably create the routes for the app - like paths
 // So the app can be navigated like from /home, to /matchup etc
 const routes: Routes = [
+  { path: '', component: IndexComponent },
+  // { path: 'wards', component: WardsComponent },
+  // { path: 'matchups', component: MatchupsComponent }
 ];
 
 @NgModule({
