8de136861ae34ab79c4f16819f2bd7c47ffc2d34
villaclara <villaclara1231@gmail.com>
2025-12-25 01:25:12 +0200

    15min timer on update matches check

--- src/api/Mybad.API/Services/HeroMatchupCacherHostedService.cs
+++ src/api/Mybad.API/Services/HeroMatchupCacherHostedService.cs
@@ -1,5 +1,6 @@
 ﻿using Mybad.Core.Services;
 using Mybad.Services.OpenDota.Cachers;
+using System.Diagnostics;
 
 namespace Mybad.API.Services;
 
@@ -8,7 +9,7 @@ public class HeroMatchupCacherHostedService : BackgroundService
 	private readonly IServiceScopeFactory _scopeFactory;
 	private readonly ILogger<HeroMatchupCacherHostedService> _logger;
 	private readonly HeroMatchupCacherStatus _status;
-	private const int _timeoutS = 3600;
+	private const int _timeoutS = 900;
 
 	public HeroMatchupCacherHostedService(IServiceScopeFactory scopeFactory, ILogger<HeroMatchupCacherHostedService> logger, HeroMatchupCacherStatus status)
 	{
@@ -46,23 +47,31 @@ public class HeroMatchupCacherHostedService : BackgroundService
 		using var scope = _scopeFactory.CreateScope();
 		var cacher = scope.ServiceProvider.GetRequiredService<ODotaHeroMatchupCacher>();
 		var notifier = scope.ServiceProvider.GetService<INotifier>();
+		var sw = Stopwatch.StartNew();
+		var opResult = false;
 
 		try
 		{
 			_logger.LogInformation("{@Method} - Start service method {@m}.", nameof(HeroMatchupCacherHostedService), nameof(DoWork));
 			await cacher.UpdateHeroMatchupsDatabase(minRank: 75);
 			_logger.LogInformation("{@Method} - End service method {@m}.", nameof(HeroMatchupCacherHostedService), nameof(DoWork));
-			if (notifier is not null)
-			{
-				await notifier.NotifyAsync(new NotifyMessage($"[{DateTime.Now} UTC]  - UpdateHeroMatchup finished.\nMatches in db - {cacher.CachedMatchesCount}."));
-			}
+			opResult = true;
 		}
 		catch (Exception ex)
 		{
 			_logger.LogWarning("{@Service} - Exception while doing work - {@ex}", nameof(HeroMatchupCacherHostedService), ex.Message);
 			if (notifier is not null)
 			{
-				await notifier.NotifyAsync(new NotifyMessage($"<b>[{DateTime.Now}]</b> - UpdateHeroMatchup failed. Exception:\n{ex.Message}."));
+				await notifier.NotifyAsync(new NotifyMessage($"<b>[{DateTime.Now} UTC]</b> - UpdateHeroMatchup failed. Exception:\n{ex.Message}."));
+			}
+		}
+		finally
+		{
+			sw.Stop();
+			if (notifier is not null)
+			{
+				await notifier.NotifyAsync(new NotifyMessage(
+					$"[{DateTime.Now} UTC] - UpdateHeroMatchup finished with success - {opResult.ToString().ToUpperInvariant()}.\nMatches in Db - {cacher.CachedMatchesCount}.\nTime elapsed - {sw.Elapsed.TotalSeconds} s."));
 			}
 		}
 	}
